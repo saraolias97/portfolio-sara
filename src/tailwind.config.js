945e90d9e7535e43c80616856f2bf966e9837faf
Sara Olías <119341710+saraolias97@users.noreply.github.com>
2025-03-28 15:29:11 +0100

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",  // Si tu proyecto tiene un archivo HTML principal
+    "./src/**/*.{js,jsx,ts,tsx}",  // Si usas archivos JS, JSX, etc.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
